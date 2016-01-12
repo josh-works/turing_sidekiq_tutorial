6e619e4dbe9fbb5a7f200ee7c8764f4840037014
Andrew Carmer <carmerandrew@gmail.com>
2016-01-12 11:03:22 -0700

    update database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160112175207) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at", null: false
