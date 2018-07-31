bfcf2b2a57cb7618e0b01a7795d8efc0ec152f67
Josh Thompson <thompsonjoshd@gmail.com>
2018-07-30 21:10:43 -0600

    finishing all the things. maybe won't touch this again for a while.

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -1,6 +1,8 @@
 class MailersController < ApplicationController
   
   def create
+    logger.debug "about to call worker"
+    # SendGifToUserWorker.new.perform(params[:mailers][:email], params[:mailers][:thought])
     SendGifToUserWorker.perform_async(params[:mailers][:email], params[:mailers][:thought])
     flash[:message] = "You did it! Email sent to #{params[:mailers][:email]}"
     redirect_to "/sent"
--- app/workers/send_gif_to_user_worker.rb
+++ app/workers/send_gif_to_user_worker.rb
@@ -2,6 +2,7 @@ class SendGifToUserWorker
   include Sidekiq::Worker
 
   def perform(email, thought)
+    logger.debug "SendGifToUserWorker debug"
     UserNotifier.send_randomness_email(email, thought).deliver_now
   end
 end
